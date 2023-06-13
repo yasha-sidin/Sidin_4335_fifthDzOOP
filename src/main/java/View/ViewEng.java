dad26b78737034d334c05c4642a92e113fdb2853
Yasha Sidin <124708836+yasha-sidin@users.noreply.github.com>
2023-06-13 15:56:19 +0300

    Update ViewEng.java

--- src/main/java/View/ViewEng.java
+++ src/main/java/View/ViewEng.java
@@ -7,23 +7,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
-public class ViewEng<T> implements iGetView<T> {
+public class ViewEng implements iGetView {
     @Override
     public void printAllStudents(List<Student> students) {
         System.out.println("------------List output------------");
         for(Object stud: (List)students) {
             System.out.println(stud);
-        }
-
-
-        System.out.println("------------End of list------------");
-    }
-
-    @Override
-    public void printAllStudents(HashMap<Long, Student> students) {
-        System.out.println("------------List output------------");
-        for(Long value : students.keySet()) {
-            System.out.println(value + " " + students.get(value));
         }
         System.out.println("------------End of list------------");
     }
