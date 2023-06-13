53eeac9a3d31a59b9e73ae31482034b793cf638e
Yasha Sidin <124708836+yasha-sidin@users.noreply.github.com>
2023-06-13 16:48:01 +0300

    Update iGetModel.java

--- src/main/java/Controller/iGetModel.java
+++ src/main/java/Controller/iGetModel.java
@@ -11,5 +11,16 @@ public interface iGetModel{
      * @return current class list of Students
      */
     public List getAllStudents();
+
+    /**
+    *
+    * @return size of model
+    */
     public int size();
+
+    /**
+    *
+    * @return delete student by his number or output info about existing of student with this number
+    */
+    public void delete(int studentNum);
 }
