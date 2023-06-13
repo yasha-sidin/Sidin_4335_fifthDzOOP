5699aa1142bc48133c4b6f84175b3c9b89ef90a6
Yasha Sidin <124708836+yasha-sidin@users.noreply.github.com>
2023-06-13 15:58:32 +0300

    Update HashModel.java

--- src/main/java/Model/HashModel.java
+++ src/main/java/Model/HashModel.java
@@ -9,8 +9,8 @@ public class HashModel implements iGetModel<HashMap> {
     public HashModel(HashMap<Long, Student> students) {
         this.students = students;
     }
-    public List<Student> getAllStudents() {
-        return students.values;
+    public List getAllStudents() {
+        return (List)students.entrySet();
     }
 
     @Override
